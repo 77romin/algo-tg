11a0b35951ae723d77352d6c8832c60906cbc513
김동준 <wpshs0418@gmail.com>
2026-07-27 08:09:46 +0900

    3주차-Hard

--- problem_solve/3주차/[Hard] [PGS] 도둑질/Nerororo.java
+++ problem_solve/3주차/[Hard] [PGS] 도둑질/Nerororo.java
@@ -1,10 +1,30 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(
-            new InputStreamReader(System.in)
-        );
+// 뭔가 좀 더러운데 아무튼 통과함..(???)
+class Solution {
+    static int len;
+    public int solution(int[] money) {
+        len = money.length;
+        int[] dp1 = new int[len];
+        int[] dp2 = new int[len];
+
+        dp1[0] = money[0];
+        dp1[1] = Math.max(money[0], money[1]);
+        dp2[1] = money[1];
+        dp2[2] = Math.max(money[1], money[2]);
+
+        int answer = 0;
+        answer = Math.max(dpCalc(dp1, money, 0), dpCalc(dp2, money, 1));
+
+        return answer;
+    }
+
+    public int dpCalc(int[] dp, int[] money, int idx) {
+        for (int i = idx + 2; i < len - 1 + idx; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + money[i]);
+        }
+
+        return dp[len - 2 + idx];
     }
 }
