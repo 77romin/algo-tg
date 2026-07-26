5e3578528fa909ed49f527545af6291763216e72
Hian02 <162945006+Hian02@users.noreply.github.com>
2026-07-27 01:39:13 +0900

    Update Hian02.java

--- problem_solve/3주차/[Normal] [PGS] 네트워크/Hian02.java
+++ problem_solve/3주차/[Normal] [PGS] 네트워크/Hian02.java
@@ -1,10 +1,31 @@
-import java.io.*;
-import java.util.*;
+class Solution {
+    public int solution(int n, int[][] computers) {
+        int[] IsConnected = new int[n];
+        int answer = 0;
 
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(
-            new InputStreamReader(System.in)
-        );
+        for (int i = 0; i < n; i++) {
+            if (IsConnected[i] == 0) {
+                answer++;
+
+                int[] stack = new int[n];
+                int top = 0;
+
+                stack[top++] = i;
+                IsConnected[i] = 1;
+
+                while (top > 0) {
+                    int current = stack[--top];
+
+                    for (int next = 0; next < n; next++) {
+                        if (computers[current][next] == 1 && IsConnected[next] == 0) {
+                            IsConnected[next] = 1;
+                            stack[top++] = next;
+                        }
+                    }
+                }
+            }
+        }
+
+        return answer;
     }
 }
