7f14a400c856e2f05ab2d88eb3db820a1dbf588e
고민균 <97932282+skyblue1232@users.noreply.github.com>
2026-08-10 23:15:28 +0900

    refactor: 창용 마을

--- problem_solve/5주차/[Easy] [SWEA] 창용 마을 무리의 개수/skyblue1232.java
+++ problem_solve/5주차/[Easy] [SWEA] 창용 마을 무리의 개수/skyblue1232.java
@@ -2,64 +2,38 @@ import java.io.*;
 import java.util.*;
 
 public class skyblue1232 {
-
-    static int N;
-    static boolean[][] map;
+    static ArrayList<Integer>[] graph;
     static boolean[] visited;
 
     public static void main(String[] args) throws Exception {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int T = Integer.parseInt(br.readLine());
 
         for (int tc = 1; tc <= T; tc++) {
-
             StringTokenizer st = new StringTokenizer(br.readLine());
-
-            N = Integer.parseInt(st.nextToken());
-            int M = Integer.parseInt(st.nextToken());
-
-            map = new boolean[N + 1][N + 1];
+            int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken());
+            graph = new ArrayList[N + 1];
             visited = new boolean[N + 1];
+            for (int i = 1; i <= N; i++) graph[i] = new ArrayList<>();
 
-            // 서로 아는 관계 입력
             for (int i = 0; i < M; i++) {
                 st = new StringTokenizer(br.readLine());
-
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-
-                // 서로 아는 관계이므로 양방향
-                map[a][b] = true;
-                map[b][a] = true;
+                int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+                graph[a].add(b); graph[b].add(a);
             }
 
             int count = 0;
-
-            // 모든 사람 확인
-            for (int i = 1; i <= N; i++) {
-
-                // 아직 어느 무리에도 포함되지 않은 사람이라면
+            for (int i = 1; i <= N; i++)
                 if (!visited[i]) {
                     dfs(i);
                     count++;
                 }
-            }
-
             System.out.println("#" + tc + " " + count);
         }
     }
 
-    static void dfs(int person) {
-
-        visited[person] = true;
-
-        for (int next = 1; next <= N; next++) {
-
-            if (map[person][next] && !visited[next]) {
-                dfs(next);
-            }
-        }
+    static void dfs(int cur) {
+        visited[cur] = true;
+        for (int next : graph[cur]) if (!visited[next]) dfs(next);
     }
 }
