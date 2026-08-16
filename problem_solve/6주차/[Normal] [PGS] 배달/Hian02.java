f7ed240e09fe5b4d12b935a21f80a32ee219f172
Hian02 <162945006+Hian02@users.noreply.github.com>
2026-08-17 03:03:29 +0900

    Update Hian02.java

--- problem_solve/6주차/[Normal] [PGS] 배달/Hian02.java
+++ problem_solve/6주차/[Normal] [PGS] 배달/Hian02.java
@@ -30,7 +30,6 @@ import java.util.*;
 
 class Solution {
     public int solution(int N, int[][] road, int K) {
-
         // graph[i] : i번 마을과 연결된 도로 정보
         // int[] = {도착 마을, 이동 시간}
         ArrayList<int[]>[] graph = new ArrayList[N + 1];
@@ -38,83 +37,56 @@ class Solution {
         for (int i = 1; i <= N; i++) {
             graph[i] = new ArrayList<>();
         }
-
-
         // 도로 정보 저장
         // 도로는 양방향이므로 양쪽 모두 저장
         for (int[] r : road) {
-
             int start = r[0];
             int end = r[1];
             int time = r[2];
-
             graph[start].add(new int[]{end, time});
             graph[end].add(new int[]{start, time});
         }
-
-
         // dist[i] = 1번 마을에서 i번 마을까지의 최소 시간
         int[] dist = new int[N + 1];
-
         // 처음에는 모든 마을까지의 거리를 매우 큰 값으로 설정
         Arrays.fill(dist, Integer.MAX_VALUE);
-
 
         // PriorityQueue
         // int[] = {마을 번호, 현재까지 걸린 시간}
         // 시간이 짧은 것부터 꺼냄
-        PriorityQueue<int[]> pq =
-                new PriorityQueue<>((a, b) -> a[1] - b[1]);
-
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
 
         // 1번 마을에서 시작
         dist[1] = 0;
         pq.offer(new int[]{1, 0});
-
-
         // 다익스트라
         while (!pq.isEmpty()) {
-
             int[] current = pq.poll();
-
             int now = current[0];
             int cost = current[1];
-
-
             // 이미 더 짧은 경로가 있다면 탐색하지 않음
             if (cost > dist[now]) {
                 continue;
             }
 
-
             // 현재 마을과 연결된 모든 도로 확인
             for (int[] next : graph[now]) {
-
                 int nextNode = next[0];
                 int nextCost = cost + next[1];
-
-
                 // 현재 마을을 거쳐가는 것이 더 빠른 경우
                 if (nextCost < dist[nextNode]) {
-
                     dist[nextNode] = nextCost;
-
                     pq.offer(new int[]{nextNode, nextCost});
                 }
             }
         }
-
-
         // K 시간 이하로 배달 가능한 마을 개수
         int answer = 0;
-
         for (int i = 1; i <= N; i++) {
-
             if (dist[i] <= K) {
                 answer++;
             }
         }
-
 
         return answer;
     }
@@ -125,7 +97,6 @@ class Solution {
 시간복잡도
 N : 마을의 개수
 M : 도로의 개수 (road.length)
-
 인접 리스트에 모든 도로를 저장하므로 O(N + M)
-최단 거리를 저장하는 dist 배열이 O(N),
+최단 거리를 저장하는 dist 배열이 O(N)
 */
