abb08f3e3f4af545939a554d20f8e599979d6b76
김세헌 <kshandy0221@naver.com>
2026-08-18 09:03:17 +0900

    Add: 6주차 합승 택시 요금 수정

--- problem_solve/6주차/[Hard] [PGS] 합승 택시 요금/NewOld21.java
+++ problem_solve/6주차/[Hard] [PGS] 합승 택시 요금/NewOld21.java
@@ -5,12 +5,12 @@ import java.util.PriorityQueue;
 
 class Solution {
 
-    static int [] answer;
+    static int answer;
     static List<List<Node>> maps;
 
 
     public int solution(int N, int s, int a, int b, int[][] fares) {
-        answer = new int[N+1] ;
+        answer = Integer.MAX_VALUE;
         maps = new ArrayList<>();
 
         for(int i=0; i<N+1; i++){
@@ -22,27 +22,23 @@ class Solution {
             maps.get(far[1]).add(new Node(far[0], far[2]));
         }
 
-        for(int i=1; i<N+1; i++) {
-            int sharing = 0;
-            if(i!=s){
-                sharing = takingTaxi(s, i, fares, N);
-            }
-            int alone_A = takingTaxi(i, a, fares, N);
-            int alone_B = takingTaxi(i, b, fares, N);
+        int[] sharing = takingTaxi(s, N);
             
-            if(sharing < 0 && alone_A < 0 && alone_B < 0){
-                continue;
-            }
-            answer[i] = sharing + alone_A +  alone_B;
+        int[] alone_A = takingTaxi(a, N);
+        int[] alone_B = takingTaxi(b, N);
+
+
+        for(int i=1; i<N+1; i++){
+            answer = Math.min(answer, sharing[i] + alone_A[i] + alone_B[i]);
         }
 
-        answer[0] = Integer.MAX_VALUE;
-        return Arrays.stream(answer).min().getAsInt();
+        return answer;
     }
 
 
-    private static int takingTaxi(int x, int y, int[][] fares, int N){
-        boolean [] visited = new boolean[N+1];
+    private static int[] takingTaxi(int x,  int N){
+        int [] visited = new int[N+1];
+        Arrays.fill(visited, Integer.MAX_VALUE);
         PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.charge, o2.charge));
 
         pq.add(new Node(x, 0));
@@ -50,26 +46,22 @@ class Solution {
         while(!pq.isEmpty()){
             Node node = pq.poll();
 
-            if(node.cur == y){
-                return node.charge;
-            }
+  
 
-            if(visited[node.cur]){
+            if(visited[node.cur]<node.charge){
                 continue;
             }
 
-            visited[node.cur] = true;
+            visited[node.cur] = node.charge;
 
             for(Node n : maps.get(node.cur)){
-                if(!visited[n.cur]){
+                if(visited[n.cur] > node.charge + n.charge){
                     pq.offer(new Node(n.cur, node.charge + n.charge));
                 }
             }
         }
-        return -1;
+        return visited;
     }
-
-
 }
 
 class Node{
@@ -81,3 +73,10 @@ class Node{
         this.charge = charge;
     }
 }
+
+
+// takingTaxi(s) → O(M log N)
+// takingTaxi(a) → O(M log N)
+// takingTaxi(b) → O(M log N)
+// 시간복잡도
+// O(3*MlogN) => O(M log N)
