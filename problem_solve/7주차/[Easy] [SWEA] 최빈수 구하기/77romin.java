1ca7c98fe839dd297bc6915e22d77cf083d45b94
MinK <bighead0831@naver.com>
2026-08-22 19:41:33 +0900

    feat: 1번 문제 - Bucket Sort

--- problem_solve/7주차/[Easy] [SWEA] 최빈수 구하기/77romin.java
+++ problem_solve/7주차/[Easy] [SWEA] 최빈수 구하기/77romin.java
@@ -4,25 +4,29 @@ class Solution {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
         StringBuilder sb = new StringBuilder();
+        
 		for(int test_case = 1; test_case <= T; test_case++) {
             int tc = sc.nextInt();
             sb.append("#").append(tc).append(" ");
-            Map<Integer, Integer> map = new TreeMap();
+            
+            int[] bucket = new int[101]; // 0<= score <= 100
+            
             for(int i=0; i<1000; i++) {
-                int n = sc.nextInt();
-                map.put(n, map.getOrDefault(n, 0)+1);
+                int score = sc.nextInt();
+                bucket[score]++;
             }
             
-            int maxAppear = 0;
-            int maxAppScore = 0;
-            for(int n : map.keySet()) {
-                if(maxAppear<=map.get(n)) {
-                    maxAppear = map.get(n);
-                    maxAppScore = n; 
+            int maxCount = 0;
+            int maxScore = 0;
+            
+            for(int score=0; score<=100; score++) {
+                if(bucket[score] >= maxCount) {
+                    maxCount = bucket[score];
+                    maxScore = score;
                 }
             }
             
-            sb.append(maxAppScore).append("\n");
+            sb.append(maxScore).append("\n");
 		}
         System.out.println(sb);
 	}
@@ -30,8 +34,9 @@ class Solution {
 
 /**
  * <Memo>
- * TreeMap을 이용해서 key값 오름차순으로 확인. (최빈값 중복 수가 존재할시 가장 큰 값 찾기 수월함)
- * 시간복잡도: O(TxNlogK) - T:testcase, N:1000, K:서로 다른 정수의 개수(최대 101개)
- * Bucket Sort를 활용하여서도 풀 예정
+ * 시간복잡도: O(N)
+ * Bucket Sort: 미리 바구니에 번호표를 붙여두고 분류해서 담는 방법
+ * 즉, 분류 후 집계하는 것이다.
+ * Map의 key-value 매핑으로, 점수별 등장 횟수를 묶어서 세는 방식과 매우 유사하다.
  */
 
