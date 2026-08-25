4d0e75376ba6fdb4720f54402fcfd5c7ee9055c0
77romin <bighead0831@naver.com>
2026-08-25 10:02:51 +0900

    feat: 3번문제풀이 - HashMap

--- problem_solve/7주차/[Normal] [PGS] 전화번호 목록/77romin.java
+++ problem_solve/7주차/[Normal] [PGS] 전화번호 목록/77romin.java
@@ -1,10 +1,23 @@
+import java.util.*;
+
 class Solution {
     public boolean solution(String[] phone_book) {
-        for(int i=0; i<phone_book.length; i++) {
-            for(int j=0; j<phone_book.length; j++) {
-                if(i!=j && phone_book[j].startsWith(phone_book[i]))
-                    return false;
+        return chkDupl(phone_book);
+    }
+    
+    private boolean chkDupl(String[] phones) {
+        Map<String, Integer> map = new HashMap<>();
+        for(String phone : phones) {
+            String[] phoneBits = phone.split("", phone.length());
+            String chkQuery = "";
+            for(int i=0; i<phoneBits.length; i++) {
+                chkQuery += phoneBits[i];
+                map.put(chkQuery, map.getOrDefault(chkQuery, 0)+1);
             }
+        }
+        for(String phone : phones) {
+            if(map.get(phone)>1)
+                return false;
         }
         return true;
     }
@@ -12,6 +25,6 @@ class Solution {
 
 /**
  * <Memo>
- * 내 식대로 풀어보기: String클래스의 startsWith함수 활용하여 문자열 포함여부 확인
- * 시간복잡도: O(N^2) -> 효율성 테스트 3~4 시간초과로 인한 실패
+ * HashMap를 활용하여 문자열의 문자토큰이 들어올때마다 해시맵에 저장하는 식으로 함. 맵에 다 저장하고 나서 각 문자열이  HashMap에서 존재하고(당연히 존재는 함) 그 값이 2번 이상일시  false 반환!
+ * 시간복잡도: O(2*N) == O(N)
  */
